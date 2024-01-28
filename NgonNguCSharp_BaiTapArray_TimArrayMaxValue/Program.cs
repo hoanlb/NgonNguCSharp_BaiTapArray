0ed6cb84e587c0a148e42c6bfcc931277cdf9442
Lâm Bình Hoàn <hoanlb1988@gmail.com>
2024-01-28 15:33:20 +0700

    Array value sum

---
+++ NgonNguCSharp_BaiTapArray_TimArrayMaxValue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgonNguCSharp_BaiTapArray_TimArrayMaxValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int arraySum = 0;
+
+            Console.WriteLine("Enter the number of elements in the array:");
+            n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            int arrayMaxValue = numbers[0];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter array element number" + i + ":");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Array is:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+                arraySum = arraySum + numbers[i];
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Array max value is: " + arraySum);
+
+            Console.ReadKey();
+        }
+    }
+}
