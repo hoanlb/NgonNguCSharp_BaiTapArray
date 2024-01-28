ec7d5726c94e4b07994656c7ee22eb70392f7f34
Lâm Bình Hoàn <hoanlb1988@gmail.com>
2024-01-28 10:04:54 +0700

    Array number sum

--- NgonNguCSharp_BaiTapArray_TinhTongCacPhanTu/Program.cs
+++ NgonNguCSharp_BaiTapArray_TinhTongCacPhanTu/Program.cs
@@ -10,6 +10,31 @@ namespace NgonNguCSharp_BaiTapArray_TinhTongCacPhanTu
     {
         static void Main(string[] args)
         {
+            int n;
+            int arrayNumbersSum = 0;
+
+            Console.WriteLine("Enter the number of elements in the array:");
+            n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter array element number" + i + ":");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Array is:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+
+                arrayNumbersSum = arrayNumbersSum + numbers[i];
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Array numbers sum is: " + arrayNumbersSum);
+
+            Console.ReadKey();
         }
     }
 }
